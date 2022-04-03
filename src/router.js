35fe0f6832ec18b2219373a2d29a68042f9ff021
Azka Hanif <azka_h_imtiyaz@telkomsel.co.id>
2022-04-03 21:23:02 +0700

    Modified router to not include headers, add icons for tab

--- src/router.js
+++ src/router.js
@@ -1,15 +1,52 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from 'screen/homeScreen';
 import FavScreen from 'screen/favScreen';
+import DetailScreen from 'screen/detailScreen';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export const HomeStack = () => {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        options={{
+          headerShown: false,
+        }}
+        name="HomeGallery"
+        component={HomeScreen}
+      />
+      <Stack.Screen name="Detail" component={DetailScreen} />
+    </Stack.Navigator>
+  );
+};
 
 /* TabBarBottom */
 export const TabBottom = () => {
   const Tab = createBottomTabNavigator();
 
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = 'home';
+          } else if (route.name === 'Favourites') {
+            iconName = 'heart';
+          }
+
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#900',
+        tabBarInactiveTintColor: 'gray',
+        headerShown: false,
+      })}>
+      <Tab.Screen name="Home" component={HomeStack} />
       <Tab.Screen name="Favourites" component={FavScreen} />
     </Tab.Navigator>
   );
